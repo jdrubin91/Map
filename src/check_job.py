c3a6775ce1e68de519e903f5d6a49e931dcaac27
jdrubin91 <jd.rubin91@gmail.com>
2016-04-20 14:46:21 -0600

    updates

--- src/check_job.py
+++ src/check_job.py
@@ -17,10 +17,10 @@ def run(job,tempdir):
             os.system("qstat " + item + " > " + tempdir + "/" + item + "_status.txt")
         status = list()
         for statusfile in [i for i in os.listdir(tempdir) if "_status.txt" in i]:
-            print statusfile
             with open(tempdir + "/" + statusfile) as F:
                 i = 0
                 for line in F:
                     i += 1
                 status.append(i)
+        os.system("rm " + tempdir + "/" + statusfile)
         boolean = sum(status) > 0
