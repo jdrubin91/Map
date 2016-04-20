23db1edf74fa88d61da552a0e5418986d8793128
jdrubin91 <jd.rubin91@gmail.com>
2016-04-20 14:29:10 -0600

    updates

--- src/check_job.py
+++ src/check_job.py
@@ -7,16 +7,17 @@ def run(job,tempdir):
     ID = list()
     with open(job) as F:
         for line in F:
-            ID.append(line)
+            ID.append(line.strip())
     print ID
     boolean = True
     while boolean:
         #time.sleep(120)
         for item in ID:
-            print item
+            print tempdir + "/" + item + "_status.txt"
             os.system("qstat " + item + " > " + tempdir + "/" + item + "_status.txt")
         status = list()
         for statusfile in [i for i in os.listdir(tempdir) if "_status.txt" in i]:
+            print statusfile
             with open(statusfile) as F:
                 i = 0
                 for line in F:
