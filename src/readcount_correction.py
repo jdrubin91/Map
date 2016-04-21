ade169aea4a4b62e4852dc8a90f99221ad7b9668
jdrubin91 <jd.rubin91@gmail.com>
2016-04-21 15:38:58 -0600

    updates

--- src/readcount_correction.py
+++ src/readcount_correction.py
@@ -3,4 +3,4 @@ __author__ = 'Jonathan Rubin'
 import os
 
 def run(scriptdir, fullpath):
-    os.system("python " + scriptdir + "/readcount_corrected_geneomeBedgraphs.py " + fullpath + "flipped/bowtie2/sortedbam/")
+    os.system("python " + scriptdir + "/readcount_corrected_geneomeBedgraphs.py " + fullpath + "flipped/bowtie2/sortedbam")
