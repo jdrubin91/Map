2fc65be134ea559dbddd0abcdeaa98ef3e0ca885
Jonathan Rubin <jonathanrubin@rl1-192-16-dhcp.int.colorado.edu>
2016-10-13 14:28:52 -0600

    updates

--- src/fastq_to_sam.py
+++ src/fastq_to_sam.py
@@ -9,7 +9,8 @@ def run(scriptdir, newpath, tempdir, genome, boolean=True):
     if boolean:
         outfile.write("mkdir -p $od\n")
     outfile.write("for pathandfilename in `ls $indir*.fastq`; do\n")
-    outfile.write("entry=`basename $pathandfilename`" + genome.split('.')[0] + "\n")
+    outfile.write("entry=`basename $pathandfilename`\n")
+    outfile.write("entry=${entry}"+genome.split('/')[-1]split('.')[0]+"\n")
     outfile.write("echo $entry\n")
     outfile.write("fq1=$entry\n")
     outfile.write("ofile=${entry}bowtie2\n")
