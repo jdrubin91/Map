4aa2a4dd95f0932510e2f63ba04e4e4a73ab04e2
Jonathan Rubin <jonathanrubin@rl1-192-16-dhcp.int.colorado.edu>
2016-10-13 15:53:40 -0600

    updates

--- src/main.py
+++ src/main.py
@@ -120,7 +120,7 @@ def run():
             check_job.run(job,tempdir)
         for file1 in [i for i in os.listdir(newpath + 'bowtie2/spikeins/') if '.stderr' in i]:
             print file1.split('.')[2]
-            with open(file1) as F:
+            with open(newpath + 'bowtie2/spikeins/' + file1) as F:
                 for line in F:
                     print line
 
