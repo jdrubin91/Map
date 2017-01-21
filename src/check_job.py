78f3f707de52ec26f816a70315d4adbecf840f2f
Jonathan Rubin <jonathanrubin@rl1-194-194-dhcp.int.colorado.edu>
2017-01-20 18:07:57 -0700

    updates

--- src/check_job.py
+++ src/check_job.py
@@ -31,8 +31,7 @@ def run(job,tempdir):
         #If there are still jobs running, print statement, then wait to check jobs again
         if boolean:
             #Clears previous print statement
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K")
+            sys.stdout.write("\r","\033[K")
             #Prints x Job(s) Still Running...
             for x in range (4):
                 b = str(sum([1 for i in status if i>0])) + " Job(s) Still Running" + "." * x
