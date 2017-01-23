452b115260cd2706e71e4e5817551dd619fe77ff
Jonathan Rubin <jonathanrubin@rl1-195-195-dhcp.int.colorado.edu>
2017-01-23 11:07:00 -0700

    updates

--- src/trim_galore.py
+++ src/trim_galore.py
@@ -6,7 +6,7 @@ def run(trimdir,newpath):
     output = newpath + 'trimmed/'
     for file1 in os.listdir(newpath):
         if 'fastq' in file1.split('.')[-1]:
-            os.system(trimdir + "trim_galore - o " + output + " " + newpath + file1)
+            os.system(trimdir + "trim_galore -o " + output + " " + newpath + file1)
     for file1 in os.listdir(output):
         if 'fq' in file1.split('.')[-1]:
             os.systme("mv " + output + file1 + " " + output + file1.split('.')[:-1] + ".fastq")
