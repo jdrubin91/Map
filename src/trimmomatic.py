ece6046613086733d64baeec15e2e62b82883d58
Jonathan Rubin <jonathanrubin@rl1-195-157-dhcp.int.colorado.edu>
2017-01-27 12:31:06 -0700

    updates

--- src/trimmomatic.py
+++ src/trimmomatic.py
@@ -6,6 +6,6 @@ def run(trimmomaticdir,newpath):
     output = newpath + 'trimmed/'
     for file1 in os.listdir(newpath):
         if 'fastq' in file1.split('.')[-1]:
-            os.system("java -jar " + trimmomaticdir + " SE " + newpath + file1 + " " + output + file1 + " ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36")
+            os.system("java -jar " + trimmomaticdir + " SE " + newpath + file1 + " " + output + file1)
 
     return output
