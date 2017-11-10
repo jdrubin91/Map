105bcb1db0b80b642b111f4b78ac936c8a841f63
Jonathan Rubin <joru1876@colorado.edu>
2017-11-09 17:38:19 -0700

    bug fixes to igvtoolstile_2.py

--- scripts/igvtoolstile_2.py
+++ scripts/igvtoolstile_2.py
@@ -6,7 +6,7 @@ import glob
 def tiledir2(genome, directory_of_BedGraphs):
 	wf = open(directory_of_BedGraphs+"igvtoolstile.sbatch", "w")
         for bedGraphfile in glob.glob(os.path.join(directory_of_BedGraphs, '*mp.BedGraph')):
-                wf.write("/opt/igvtools/2.3.75/igvtools toTDF "+bedGraphfile+" "+bedGraphfile+".tdf /opt/igvtools/2.1.24/genomes/" + genome + ".genome &\n\n")
+                wf.write("/opt/igvtools/2.3.75/igvtools toTDF " + bedGraphfile + " " + bedGraphfile + ".tdf /opt/igvtools/2.3.75/genomes/" + genome + ".chrom.sizes\n")
 	wf.close()
 
 
